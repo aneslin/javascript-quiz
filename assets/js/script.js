feb0b8be123a6e157836fe4a59e24ed686e660e5
Aaron Neslin <aneslin125@gmail.com>
2021-12-16 12:44:48 -0500

    got button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-var timerEl = document. getElementById("time")
-var question = document.createElement("h3")
-var answer1Ul = document.createElement("ul")
-
+var timerEl = document. getElementById("time");
+var question = document.createElement("h3");
+var answer1Ul = document.createElement("ul");
+let timeleft;
 //create loop for array of choices
 
 var questionMainEl = document.getElementById("greeting-box");
 let questionSection = document.getElementById("question-section");
 
-let sectionArray =[questionMainEl, questionSection]
 
 const questionArray = [
  question1 =  {"question": "who is the best dog?", 
@@ -25,52 +24,63 @@ question2 ={"question": "what is the best toy?",
   "What I am not supposed to have"
 ],
 "correct": "What I am not supposed to have"
+}]
+
+//hide sections from the user
+function hideSection(elementId){
+    var visibleElement = document.getElementById(elementId);
+    visibleElement.classList.add("hide")
 }
-
-]
-
-
-function hideSections(){
-  for (let i = 0; i < sectionArray.length; i++) {
-      if(!sectionArray[i].classList.contains('hide')){
-        sectionArray[i].classList.add('hide')
-      }
-  }
+//display sections to the user
+function showSections(elementId){
+    var visibleElement = document.getElementById(elementId);
+    visibleElement.classList.remove("hide")
 }
-
+//counter function
 function count(){
-  timeleft =30;
-
-  var time = setInterval(function(){
-    timerEl.textContent = timeleft;
-   if (timeleft===0){
-  clearInterval(time)
+    timeleft=30
+    var time = setInterval(function(){
+      timerEl.textContent = timeleft;
+        if (timeleft===0){
+        clearInterval(time)
   //call score page
 } 
 //or if question array i = questionArray length
 else {
     timeleft --}
-  },1000)
+  },1000);
 } ;
 
+function answerMaker(question,parentId){
 
 
- function start (){
-count();
-hideSections();
-for(var i = 0; i <  questionArray.length; i++){
-  console.log(questionArray[i])
+  for(i = 0; i < question.choices.length; i ++){
+    var listItemEl = document.createElement("li");
+    var buttonEl = document.createElement("button");
+    buttonEl.elementId= "answer-" + i;
+    buttonEl.textContent=question.choices[i]
+    listItemEl.appendChild(buttonEl)
+  outputEl = document.getElementById(parentId)
+  outputEl.appendChild(listItemEl)
+  }
+
+}
+
+ var start = function  (){
+    hideSection("greeting-box");
+    showSections("question-section")
+  count();
+  
+  var iQ = 0
+    var questionEl = document.getElementById("question-section")
+    var questionTextEl = document.createElement("h3")
+    console.log(questionArray[iQ].question)
+    questionTextEl.textContent = questionArray[iQ].question
+    questionEl.appendChild(questionTextEl)
+    
+  }
+
   //create li and button 
 
   //event listerner on button. if button === {"correct"} index +1, else timer -10 AND index +1 
-}
-
-questionSection.classList.remove('hide')
-
-
-}
-
-
-
-
-//document.getElementById("startBtn").addEventListener("click", start())
+document.getElementById("startBtn").addEventListener("click", start)
