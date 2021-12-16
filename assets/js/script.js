5649ba824c6d33b066cf9cd37396fbebdc2a9a48
Aaron Neslin <aneslin125@gmail.com>
2021-12-16 14:39:19 -0500

    added recursive function to delete and advance

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var timerEl = document. getElementById("time");
 var question = document.createElement("h3");
 var answer1Ul = document.createElement("ul");
 let timeleft;
+var iQ = 0
 //create loop for array of choices
 
 var questionMainEl = document.getElementById("greeting-box");
@@ -51,34 +52,53 @@ else {
   },1000);
 } ;
 
-function answerMaker(question,parentId){
-
-
+function answerMaker(question){
+  var answerBlockEl = document.createElement("ul")
   for(i = 0; i < question.choices.length; i ++){
     var listItemEl = document.createElement("li");
     var buttonEl = document.createElement("button");
     buttonEl.elementId= "answer-" + i;
+    console.log(question.choices[i])
     buttonEl.textContent=question.choices[i]
     listItemEl.appendChild(buttonEl)
-  outputEl = document.getElementById(parentId)
-  outputEl.appendChild(listItemEl)
+    answerBlockEl.appendChild(listItemEl)
   }
-
+  return answerBlockEl
 }
+
+
 
  var start = function  (){
     hideSection("greeting-box");
     showSections("question-section")
   count();
+  var questionEl = document.getElementById("question-section")
   
-  var iQ = 0
-    var questionEl = document.getElementById("question-section")
+  
+  
+  function quiz(){
+    var questionWrapperEl = document.createElement("div")
+    questionWrapperEl.elementId = "question-wrapper"
+    questionEl.appendChild(questionWrapperEl)
     var questionTextEl = document.createElement("h3")
     console.log(questionArray[iQ].question)
     questionTextEl.textContent = questionArray[iQ].question
-    questionEl.appendChild(questionTextEl)
-    
+    questionWrapperEl.appendChild(questionTextEl)
+    questionWrapperEl.appendChild(answerMaker(questionArray[iQ]))
+    document.getElementById("question-section").addEventListener("click", function(answerChoice){
+      if (answerChoice.target.textContent != questionArray[iQ].correct){
+        timeleft = timeleft - 10
+
+      document.getElementById("question-section").replaceChildren([''])
+      iQ++
+      console.log(iQ)
+      quiz()
+      }
+     
+    })
   }
+  quiz()
+}
 
   //create li and button 
 
